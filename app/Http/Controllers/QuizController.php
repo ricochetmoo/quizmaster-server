a5413425b099f83bf6d572e731b777bae1b220c4
James Barber <james@jamesbarber.tech>
2021-10-31 20:25:27 +0000

    Added models and QuizController

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+	public function showAll()
+	{
+		return response()->json(Quiz::all());
+	}
+
+	public function create(Request $request)
+	{
+		$quiz = Quiz::create($request->all());
+
+		return response()->json($quiz, 201);
+	}
+
+	public function showOne($id)
+	{
+		return response()->json(Quiz::find($id));
+	}
+
+	public function update(Request $request, $id)
+	{
+		$quiz = Quiz::findOrFail($id);
+		$quiz->update($request->all());
+
+		return response()->json($quiz, 200);
+	}
+
+	public function delete($id)
+	{
+		Quiz::findOrFail($id)->delete;
+
+		return response('Deleted successfully.', 200);
+	}
+}
